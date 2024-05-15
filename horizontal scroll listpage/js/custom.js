ea4d432564b745c6960135fca022d4b0db4aab47
hyeyoung31 <154103645+hyeyoung31@users.noreply.github.com>
2024-05-15 23:21:38 +0900

    first commit

---
+++ horizontal scroll listpage/js/custom.js
@@ -0,0 +1,54 @@
+﻿$(function(){
+    const numAc =  $("article").size();
+    const  widSec = 200*numAc;
+    const widTotal = widSec+600; 
+
+    $("section").width(widTotal);
+    $("body").height(widSec);	
+
+    $(window).on("scroll",function(){
+        const scroll = $(this).scrollTop();		
+        $("section").stop().animate({"left":-scroll},600);
+    });
+
+    $("article h2").on("click",function(e){
+        e.preventDefault();
+        const index = $(this).parent().index();
+        const src = $(this).children("a").attr("href");	
+        const posAc = 200*index;
+
+        $("article").removeClass("on");		
+        $(this).parent().addClass("on");
+        $("article p img").attr({"src":""});
+        $(this).siblings("p").children("img").attr({"src":src});	
+        $("html,body").scrollTop(posAc);
+    });    
+
+    $("span").on("click",function(){
+        $("article").removeClass("on");
+      });
+    
+      $("#navi li").on("click",function(){
+        const i = $(this).index();
+        const posNavi =1000*i;	
+        $("#navi li, article").removeClass();
+        $(this).addClass("on");	
+        $("html,body").scrollTop(posNavi);	
+       });  
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
